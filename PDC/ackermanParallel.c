b5427b96f9e232f64e7b96bf57faf276a4dd3000
salt57 <sourishgupta02@gmail.com>
2023-01-18 23:20:50 +0530

    feat: pdc programs

--- PDC/ackermanParallel.c
+++ PDC/ackermanParallel.c
@@ -6,32 +6,77 @@
 int ack(int m, int n)
 {
     int result, presult;
+#pragma parallel shared(result, presult) private(m, n)
+    {
+        if (m == 0)
+        {
+            result = n + 1;
+        }
+        else if (n == 0)
+        {
+#pragma omp task shared(result)
+            {
+                result = ack(m - 1, 1);
+            }
+        }
+        else
+        {
+#pragma omp task shared(presult)
+            {
+                presult = ack(m, n - 1);
+            }
+
+#pragma omp taskwait
+            {
+                result = ack(m - 1, presult);
+            }
+        }
+    }
+    return result;
+}
+
+int ackSeq(int m, int n)
+{
+    int result, presult;
     if (m == 0)
     {
         result = n + 1;
     }
     else if (n == 0)
     {
-        result = ack(m - 1, 1);
+        result = ackSeq(m - 1, 1);
     }
     else
     {
-#pragma parallel shared(result, presult) private(m, n)
-        {
-#pragma omp task shared(presult)
-            {
-                presult = ack(m, n - 1);
-            }
-#pragma omp taskwait
-            result = ack(m - 1, presult);
-        }
+        result = ackSeq(m - 1, ackSeq(m, n - 1));
     }
     return result;
 }
 
 int main(int argc, char const *argv[])
 {
-    printf("%d ", ack(8, 5));
+    omp_set_num_threads(4);
+    double t1, t2, s1, s2;
+    t1 = omp_get_wtime();
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j < 11; j++)
+        {
+            printf("%d\n", ack(i, j));
+        }
+    }
+    t2 = omp_get_wtime();
+    printf("parallel time = %g", (t2 - t1));
+    s1 = omp_get_wtime();
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j < 11; j++)
+        {
+            printf("%d\n", ackSeq(i, j));
+        }
+    }
+    s2 = omp_get_wtime();
+    printf("sequential time = %g", (s2 - s1));
 
     return 0;
 }
